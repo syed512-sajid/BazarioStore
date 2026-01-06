7191228d5b7600b514b49ab8920dc6ea1bdb0b9d
Sajid Abbas <134507802+syed512-sajid@users.noreply.github.com>
2026-01-06 16:05:59 +0500

    Update EmailSettings.cs

--- Models/EmailSettings.cs
+++ Models/EmailSettings.cs
@@ -1,11 +1,12 @@
-﻿namespace EcommerceStore.Models
+namespace EcommerceStore.Models
 {
     public class EmailSettings
     {
-        public string Host { get; set; } = "";
-        public int Port { get; set; }
-        public string Email { get; set; } = "";
-        public string Password { get; set; } = "";
+        public string SmtpHost { get; set; } = "";
+        public int SmtpPort { get; set; }
+        public string SmtpUser { get; set; } = "";
+        public string SmtpPass { get; set; } = "";
         public string FromName { get; set; } = "";
+        public string FromEmail { get; set; } = "";
     }
 }
