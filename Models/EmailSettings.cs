66cae20cdebfa407bab05cf940122144da746307
Sajid Abbas <134507802+syed512-sajid@users.noreply.github.com>
2026-01-07 16:50:28 +0500

    Update EmailSettings.cs

--- Models/EmailSettings.cs
+++ Models/EmailSettings.cs
@@ -2,11 +2,7 @@ namespace EcommerceStore.Models
 {
     public class EmailSettings
     {
-        public string SmtpHost { get; set; } = "smtp.gmail.com";
-        public int SmtpPort { get; set; } = 465;
-        public string SmtpUser { get; set; } = "";
-        public string SmtpPass { get; set; } = "";
         public string FromName { get; set; } = "BAZARIO Store";
-        public string FromEmail { get; set; } = "";
+        public string FromEmail { get; set; } = "onboarding@resend.dev"; // default Resend "from"
     }
 }
