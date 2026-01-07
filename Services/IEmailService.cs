d7605523e7493da2212a6aeef0d3bdc7ce65a3c6
Sajid Abbas <134507802+syed512-sajid@users.noreply.github.com>
2026-01-07 17:01:50 +0500

    Update IEmailService.cs

--- Services/IEmailService.cs
+++ Services/IEmailService.cs
@@ -1,1 +1,13 @@
 
+using EcommerceStore.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace EcommerceStore.Services
+{
+    public interface IEmailService
+    {
+        Task SendOrderConfirmationAsync(Order order, List<CartItem> cart);
+        Task SendAdminNotificationAsync(Order order, List<CartItem> cart);
+    }
+}
