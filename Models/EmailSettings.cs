b176555601cf250688d6d3391df6e1a4843eeadf
Sajid Abbas <134507802+syed512-sajid@users.noreply.github.com>
2026-01-07 16:59:52 +0500

    Update EmailSettings.cs

--- Models/EmailSettings.cs
+++ Models/EmailSettings.cs
@@ -3,6 +3,6 @@ namespace EcommerceStore.Models
     public class EmailSettings
     {
         public string FromName { get; set; } = "BAZARIO Store";
-        public string FromEmail { get; set; } = "onboarding@resend.dev"; // default Resend "from"
+        public string FromEmail { get; set; } = "onboarding@resend.dev";
     }
 }
